96137faf8756b8ccf3e7d0e0bb750893640962d0
ramli <rizalramli@its.ac.id>
2023-05-08 10:13:21 +0700

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rizalramli/go-say-hello
+module github.com/rizalramli/go-say-hello/v2
 
 go 1.18
--- say_hello.go
+++ say_hello.go
@@ -1,9 +1,13 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
 
 // Release tag
 // 1. git tag v1.0.0
 // 2. git push origin v1.0.0
+
+// Jika terjadi major upgrade
+// 1. misal function ada tambahan porameter
+// 2. Ubah nama module di akhir menjadi /v2
